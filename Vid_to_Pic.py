796d8f310d62ffdeb1a101c2516afd05b720779d
MikraMikra <krav.michael.ba@gmail.com>
2023-11-15 11:19:56 +0100

    Added the argument "-size". Now it is possible to chose the size of the picture.

--- Vid_to_Pic.py
+++ Vid_to_Pic.py
@@ -1,10 +1,11 @@
-#req: moviepy
-
 from moviepy.editor import VideoFileClip
+from moviepy.video.VideoClip import ImageClip
 import os
 import argparse
+from PIL import Image
 
-def convert_video_to_images(input_video, output_folder):
+
+def convert_video_to_images(input_video, output_folder, image_size):
     # Create the output folder if it does not exist
     if not os.path.exists(output_folder):
         os.makedirs(output_folder)
@@ -15,7 +16,12 @@ def convert_video_to_images(input_video, output_folder):
     # Iterate over every second of the video and save an image
     for i, frame in enumerate(video_clip.iter_frames(fps=1, dtype='uint8')):
         image_file = os.path.join(output_folder, f'frame_{i + 1}.jpg')
-        video_clip.save_frame(image_file, t=i)
+        img_clip = ImageClip(frame)
+
+        if image_size:
+            img_clip = img_clip.resize((image_size[0], image_size[1]))
+
+        img_clip.save_frame(image_file, withmask=False)
 
     # close the video
     video_clip.close()
@@ -25,9 +31,17 @@ def main():
     parser = argparse.ArgumentParser(description='Convert a video to images.')
     parser.add_argument('--source', required=True, help='Path to the input video file.')
     parser.add_argument('--output', required=True, help='Path to the output folder for images.')
+    parser.add_argument('--size', default=None, type=str,
+                        help='Size of the output images (widthxheight). Example: 640x480')
 
     args = parser.parse_args()
 
-    convert_video_to_images(args.source, args.output)
+    image_size = None
+    if args.size:
+        image_size = tuple(map(int, args.size.split('x')))
 
-main()
+    convert_video_to_images(args.source, args.output, image_size)
+
+
+if __name__ == "__main__":
+    main()
